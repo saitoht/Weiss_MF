4e49252681410dc15511f01fba812842cfa119db
saitoht <harujie6@gmail.com>
2024-09-10 08:44:53 +0900

    add description

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Magntic Gibbs Free Energy based on Weiss Mean-Field Theory
 
+## Requirements for Python
+- Numpy
+- Matplotlib
+
 ## Example for BCC Fe
+Before calculation, you should see the contents of Weiss.py.
+First in main() function, you will define input parameters ndiv, magfield, colors, Tc, nmag, struc for the system under consideration.
+
 You can calculate the property by the following command.
 ```bash
 python Weiss.py 2.2 -p
@@ -10,16 +17,16 @@ Here, 2.2 means the magnetic moment of Fe by DFT.
 
 ### Result figures
 - Moment.pdf ... Magnetic moment vs temperature
-![](figs/Moment.pdf)
+![](figs/Moment.png)
 
 - Cm.pdf ... Specific heat vs temperature
-![](figs/Cm.pdf)
+![](figs/Cm.png)
 
 - dHm.pdf ... Entalphy vs temperature
-![](figs/dHm.pdf)
+![](figs/dHm.png)
 
 - dSm.pdf ... Entropy vs temperature
-![](figs/dSm.pdf)
+![](figs/dSm.png)
 
 - dGm.pdf ... Magnetic Gibbs free energy vs tempearture
-![](figs/dGm.pdf)
+![](figs/dGm.png)
--- Weiss.py
+++ Weiss.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import sys, os
 
 R = 8.3144569919459 # J/mol K
@@ -8,54 +7,12 @@ kB = 8.6173303e-5 # eV/K
 T2J = 5.584937543138665 # J/mol T
 mu_B = 5.7883817982e-05 # eV/T
 
-def coth(x):
-    if (np.abs(x) < 1e-20):
-        return 0.
-    else:
-        return 1./np.tanh(x)
-
-def Brillouin(j, alpha):
-    """ define Brillouin function   Eq. (S4) """
-    cf = 0.5*((2*j+1)/j)
-    cfp = 0.5/j
-    Bf = cf*coth(cf*alpha) - cfp*coth(cfp*alpha)
-    return Bf
-
-def mmom(temp, magfield, mmom0, lamb, mmonb):
-    """ return magnetic moment at T, B    Eq. (S2) """
-    alpha = mmom0*mu_B * (magfield + lamb*mmonb*mu_B)/(kB*temp)  ### Eq. (S3)
-    return mmom0 * Brillouin(0.5*mmom0, alpha)
-
-def fs(struc):
-    if ( struc == 'BCC' ):
-        return 0.285
-    elif ( struc == 'FCC' ):
-        return 0.105 
-
-def kf(mm, struc):
-    cf = 4.*(1.-fs(struc))/(1.-np.exp(-4.))
-    return cf*kB*np.log(mm+1.)
-
-def kp(mm, struc, p=2.):
-    return 8.*p*fs(struc)*kB*np.log(mm+1.)
-
-def Cmcalc(tempc, magfield, mm, struc, p=2.):
-    if (tempc >= 1.):
-        return kp(mm,struc,p=p)*tempc*np.exp(8.*p*(1.-tempc))
-    else:
-        return kf(mm,struc)*tempc*np.exp(-4.*(1.-tempc))
-
-def Simpson(x, f, n):
-    return sum([(x[i+1]-x[i])*(f[i]+4.*f[i+1]+f[i+2])/3. for i in range(0,n)])
-
-def Tc2lambda(J, Tc, n, g=2.):
-    return 3.*kB*Tc/(J*(J+1.)*n*(g**2)*(mu_B**2))  # T/eV
-
-if __name__=='__main__':
+def main():
     """ m0: magnetic moment at T=0k, B=0T from DFT (mu_B)
         temp: temperature (K)
         Tc: Curie temperature (K)
         ndiv: number of devision for temperature
+        colors: colors to plot physical quantities, should be the same dimension as magfield
         nmag: number of magnetic atom in the unit cell
         lamb: lambda, molecular field const 
         magfield: magnetic field B (T) 
@@ -132,3 +89,50 @@ if __name__=='__main__':
         plt.savefig("dGm.pdf")
         plt.legend()
         plt.show()
+
+
+def coth(x):
+    if (np.abs(x) < 1e-20):
+        return 0.
+    else:
+        return 1./np.tanh(x)
+
+def Brillouin(j, alpha):
+    """ define Brillouin function   Eq. (S4) """
+    cf = 0.5*((2*j+1)/j)
+    cfp = 0.5/j
+    Bf = cf*coth(cf*alpha) - cfp*coth(cfp*alpha)
+    return Bf
+
+def mmom(temp, magfield, mmom0, lamb, mmonb):
+    """ return magnetic moment at T, B    Eq. (S2) """
+    alpha = mmom0*mu_B * (magfield + lamb*mmonb*mu_B)/(kB*temp)  ### Eq. (S3)
+    return mmom0 * Brillouin(0.5*mmom0, alpha)
+
+def fs(struc):
+    if ( struc == 'BCC' ):
+        return 0.285
+    elif ( struc == 'FCC' ):
+        return 0.105 
+
+def kf(mm, struc):
+    cf = 4.*(1.-fs(struc))/(1.-np.exp(-4.))
+    return cf*kB*np.log(mm+1.)
+
+def kp(mm, struc, p=2.):
+    return 8.*p*fs(struc)*kB*np.log(mm+1.)
+
+def Cmcalc(tempc, magfield, mm, struc, p=2.):
+    if (tempc >= 1.):
+        return kp(mm,struc,p=p)*tempc*np.exp(8.*p*(1.-tempc))
+    else:
+        return kf(mm,struc)*tempc*np.exp(-4.*(1.-tempc))
+
+def Simpson(x, f, n):
+    return sum([(x[i+1]-x[i])*(f[i]+4.*f[i+1]+f[i+2])/3. for i in range(0,n)])
+
+def Tc2lambda(J, Tc, n, g=2.):
+    return 3.*kB*Tc/(J*(J+1.)*n*(g**2)*(mu_B**2))  # T/eV
+
+if __name__=='__main__':
+    main()
